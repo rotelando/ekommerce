e5d68af8b9d19ad41d66d2402644dd5680762700
Akintewe Oluwarotimi <akintewe.rotimi@gmail.com>
2014-08-09 12:02:50 +0100

    setting up database

--- app/config/database.php
+++ app/config/database.php
@@ -54,9 +54,9 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'host'      => 'ekommerce.dev',
+			'database'  => 'konga_customers',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
